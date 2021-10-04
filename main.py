5da939f234bf967347b07eceaf35003a4f6aac46
Alden <aldensmith06@gmail.com>
2021-10-03 23:21:01 -0700

    Full functionality completed.
    Added SQL Querying.
    Added QR code generation and reading.
    Added emailing.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import pyzbar.pyzbar as pyzbar
 
 import qrcode  # pip python library for generating qr codes
 import smtplib  # Default Python library for emails
@@ -7,6 +8,10 @@ from email.message import EmailMessage
 
 import cv2
 
+from datetime import datetime
+
+
+SECONDS_IN_AN_HOUR = 3600
 
 def main():
     running = True
@@ -36,7 +41,6 @@ def should_continue():
 
 
 def send_mail(image, nau_username):
-
     send_to = f"{nau_username}@nau.edu"
 
     message = "This is your personal QR code. You will use this in the future to sign into the acm meetings and earn " \
@@ -45,7 +49,7 @@ def send_mail(image, nau_username):
 
     msg = EmailMessage()
     msg["Subject"] = "Welcome to ACM"
-    msg["From"] = "aldensbotalerts@gmail.com"
+    msg["From"] = "" # bot usr
     msg["To"] = send_to
     msg.set_content(message)
 
@@ -58,7 +62,7 @@ def send_mail(image, nau_username):
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:  # Identifies us with the mail server & Encrypt traffic
 
-        smtp.login("", "") # Bot account login
+        smtp.login("", "")  # Bot account login
 
         smtp.send_message(msg)
 
@@ -74,23 +78,86 @@ def create_acc():
     img = qrcode.make(str(nau_username))  # create out qr code
     img.save(qr_file_name)  # Save it with its name
 
-    db = mysql.connector.connect(
-        host="acmmembers.mysql.database.azure.com",
-        user="acmadmin@acmmembers",
-        passwd="ACMst@rtsNOW!",
-        database="nauacm",
-    )
+    db = connect_to_db()
 
     my_cursor = db.cursor()
 
-    insert_statement = "INSERT INTO AcmMembers (Username, first_name, tickets) VALUES (%s, %s, %s)"
-    data = (nau_username, first_name, 1)  # Grant 1 ticket for attending initial meeting
+    insert_statement = "INSERT INTO AcmMembers (Username, first_name, tickets, last_sign_in_date) VALUES (%s, %s, %s, %s)"
+    data = (nau_username, first_name, 1, datetime.now())  # Grant 1 ticket for attending initial meeting
 
     my_cursor.execute(insert_statement, data)  # Use this to execute queries
     db.commit()
     send_mail(qr_file_name, nau_username)
 
+
 def sign_in():
-    print("test")
+    cap = cv2.VideoCapture(0)
+
+    nauid = ""
+
+    db = connect_to_db()
+
+    data_received = False
+
+    while not data_received:  # make while data not received
+        _, frame = cap.read()
+
+        decoded_objects = pyzbar.decode(frame)
+
+        for obj in decoded_objects:
+            b = obj.data  # data in type bytes
+            data = b.decode('utf-8')  # data now a string
+            nauid += data
+            data_received = True
+
+        cv2.imshow("Frame", frame)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    my_cursor = db.cursor()
+
+    # get date from last sign in
+    # if the date > 20.0 from now allow sign in, else say you cant and leave
+
+    my_cursor.execute(f"Select last_sign_in_date FROM acmmembers where username = '{nauid}'")
+
+    result = my_cursor.fetchone()
+
+    last_sign_in_date = ""
+
+    for row in result:
+        last_sign_in_date = row
+
+    now = datetime.now()
+
+    duration = now - last_sign_in_date
+    duration_in_seconds = duration.total_seconds()
+    hours_since_last_sign_in = duration_in_seconds / SECONDS_IN_AN_HOUR # we only care about hours so drop the integer.
+
+    hours_since_last_sign_in = float(format(hours_since_last_sign_in, ".2"))
+
+    if hours_since_last_sign_in > 20.0:
+        update_statement = "UPDATE acmmembers SET tickets = tickets + 1, last_sign_in_date = %s WHERE username = %s"
+        statement_data = (now, nauid)
+
+        my_cursor.execute(update_statement, statement_data)
+        db.commit()
+
+        print("Signed in, Earned 1 point for attending today!")
+    else:
+        print("You have signed in too recently.")
+        print(f"Last sign in was {hours_since_last_sign_in} hours ago.")
+
+
+def connect_to_db():
+    db = mysql.connector.connect(
+        host="acmmembers.mysql.database.azure.com",
+        user="",
+        passwd="",
+        database="nauacm",
+    )
+    return db
+
 
 main()
